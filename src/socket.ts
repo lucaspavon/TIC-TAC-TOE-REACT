7aaab808225e7e8cd8bb2f7ee76294279d81c6c4
Lucas Pavon <lucas.pavon91@hotmail.com>
2025-07-07 15:24:50 -0300

    cambiar configuracion del start

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://tic-tac-toe-node-wicket.onrender.com:3000';
+const URL = 'https://tic-tac-toe-node-wicket.onrender.com/';
 export const socket = io(URL);
